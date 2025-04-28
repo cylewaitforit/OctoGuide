e05f6d1ca5bc90201379e20a4a20a385b74b0802
Josh Goldberg <git@joshuakgoldberg.com>
2025-04-28 09:00:26 -0400

    chore: remove empty issue-required-fields-content rule

--- src/rules/all.ts
+++ src/rules/all.ts
@@ -1,7 +1,6 @@
 import type { Rule } from "../types/rules.js";
 
 import { commentMeaningless } from "./commentMeaningless.js";
-import { issueRequiredFieldsContent } from "./issueRequiredFieldsContent.js";
 import { prBranchNonDefault } from "./prBranchNonDefault.js";
 import { prLinkedIssue } from "./prLinkedIssue.js";
 import { prTaskCompletion } from "./prTaskCompletion.js";
@@ -10,7 +9,6 @@ import { textImageAltText } from "./textImageAltText.js";
 
 export const rules: Rule[] = [
 	commentMeaningless,
-	issueRequiredFieldsContent,
 	prBranchNonDefault,
 	prLinkedIssue,
 	prTaskCompletion,
